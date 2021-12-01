4db8b16b829f66ae7432590274743de252a2f25f
Robert.Fornkahl <rwfornkahl@gmail.com>
2021-12-01 17:50:35 -0600

    updated require statment

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 // Write your JavaScript code here!
-import {createRequire} from 'module';
 
-const { formSubmission, myFetch, pickPlanet, addDestinationInfo } = require("./scriptHelper");
+
+/*const addDestinationInfo = require('./scriptHelper.js');
+const pickPlanet = require('./scriptHelper.js'); 
+const myFetch = require('./scriptHelper.js');
+const formSubmission = require('./scriptHelper.js');
 
 window.addEventListener("load", function() {
 
-    formSubmission();
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse = fetch("https://handlers.education.launchcode.org/static/planets.json");
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.+
-       
-       let pickedPlanet = pickPlanet(listedPlanets);
-   addDestinationInfo(pickedPlanet);
+    formSubmission(document);
+
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
+        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     })
    
 });
+*/
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,6 +1,6 @@
 // Write your helper functions here!
-import {createRequire} from 'module';
-require('isomorphic-fetch');
+
+//require('isomorphic-fetch');
 
 if (typeof window !== "undefined"){
 window.addEventListener("load", function(){
@@ -8,26 +8,22 @@ window.addEventListener("load", function(){
 
 
 
+    function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+      // Here is the HTML formatting for our mission target div.
+      /*
+                   <h2>Mission Destination</h2>
+                   <ol>
+                       <li>Name: </li>
+                       <li>Diameter: </li>
+                       <li>Star: ${star}</li>
+                       <li>Distance from Earth: </li>
+                       <li>Number of Moons: </li>
+                   </ol>
+                   <img src="">
+      */
+   }
+   
 
-
-function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-    const destitnation = document.getElementById("missionTarget");
-    index = pickPlanet();
-   destitnation.innerHTML = 
-   // Here is the HTML formatting for our mission target div.
-   `
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: ${json[index].name} </li>
-                    <li>Diameter: ${json[index].diameter}</li>
-                    <li>Star: ${json[index].star}</li>
-                    <li>Distance from Earth: ${json[index].distance} </li>
-                    <li>Number of Moons: ${json[index].moons} </li>
-                </ol>
-                <img src="json.image">
-   `;
-   return addDestinationInfo(document);
-}
 
 
     
@@ -92,13 +88,8 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
          event.preventDefault();
       }
    });
-   return formSubmission(document);
+   
 };
-
-
-
-
-
 
 
 async function myFetch() {
@@ -123,15 +114,46 @@ function pickPlanet(planets) {
     return planets;
 }
 
+//window.addEventListener("load", function() {
+
+   formSubmission(document);
+
+   let listedPlanets;
+   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+   let listedPlanetsResponse = myFetch("https://handlers.education.launchcode.org/static/planets.json");
+   listedPlanetsResponse.then(function (result) {
+       listedPlanets = result;
+       console.log(listedPlanets);
+   }).then(function () {
+       console.log(listedPlanets);
+       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+      let pickedPlanet = pickPlanet(listedPlanets);
+      function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl){
+         const div = document.getElementById("missionTarget");
+         div.innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${JSON[pickedPlanet].name}</li>
+            <li>Diameter: ${JSON[pickedPlanet].diameter}</li>
+            <li>Star: ${JSON[pickedPlanet].star}</li>
+            <li>Distance from Earth: ${JSON[pickedPlanet].distance}</li>
+            <li>Number of Moons: ${JSON[pickedPlanet].moons}</li>
+         </ol>
+         <img src="${JSON[pickedPlanet].image}">
+         `;
+       };
+       addDestinationInfo(document);
+      })
+  
+//});
 
 
-});
-}
-
+/*
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
 module.exports.pickPlanet = pickPlanet; 
 module.exports.myFetch = myFetch;
-
-
+*/
+});
+}
